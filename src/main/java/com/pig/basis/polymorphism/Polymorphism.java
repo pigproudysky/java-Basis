cb7f5df159478e316cb3ba098fd80c822fa048f2
bolong <bolong1017@163.com>
2019-07-29 10:20:20 +0800

    update

---
+++ src/main/java/com/pig/basis/polymorphism/Polymorphism.java
@@ -0,0 +1,16 @@
+package com.pig.basis.polymorphism;
+
+import java.io.*;
+import java.math.BigDecimal;
+
+/**
+ * @author bolong
+ * @project-name java-Basis
+ * @create 2019/7/26
+ */
+public abstract class Polymorphism {
+
+    public static void main(String[] args) {
+    }
+
+}
